47b44a92c661274a3dbe1de93d024b293f9fd452
Harsh Shandilya <msfjarvis@gmail.com>
2017-06-26 11:47:07 +0530

    s/"/\'/g

--- delete-aosp.py
+++ delete-aosp.py
@@ -4,10 +4,10 @@ import subprocess
 
 # in-place prettyprint formatter
 def indent(elem, level=0):
-    i = "\n" + level*"  "
+    i = '\n' + level*'  '
     if len(elem):
         if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
+            elem.text = i + '  '
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
@@ -20,26 +20,26 @@ def indent(elem, level=0):
 
 manifest = subprocess.check_output(['repo','manifest'])
 xml_root = ElementTree.fromstring(manifest)
-lm = ElementTree.Element("manifest")
+lm = ElementTree.Element('manifest')
 projects = xml_root.findall('project')
 defaults = xml_root.find('default')
 if defaults.get('remote') == 'github': # Gah, it's LineageOS. No remote specified == Lineage github.
     for project in projects:
         remote = project.get('remote')
         if remote == 'aosp':
-            lm.append(ElementTree.Element("remove-project", attrib = {
-                "name": project.get('name')
+            lm.append(ElementTree.Element('remove-project', attrib = {
+                'name': project.get('name')
             }))
 else:
     for project in projects:
         remote = project.get('remote')
     if not remote:
-        lm.append(ElementTree.Element("remove-project", attrib = {
-            "name": project.get('name')
+        lm.append(ElementTree.Element('remove-project', attrib = {
+            'name': project.get('name')
         }))
 indent(lm, 0)
 raw_xml = ElementTree.tostring(lm)
-raw_xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + raw_xml.decode("utf-8")
+raw_xml = '<?xml version='1.0' encoding='UTF-8'?>\n' + raw_xml.decode('utf-8')
 f = open('delete_aosp.xml', 'w')
 f.write(raw_xml)
 f.close()
--- delete-linux.py
+++ delete-linux.py
@@ -4,10 +4,10 @@ import subprocess
 
 # in-place prettyprint formatter
 def indent(elem, level=0):
-    i = "\n" + level*"  "
+    i = '\n' + level*'  '
     if len(elem):
         if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
+            elem.text = i + '  '
         if not elem.tail or not elem.tail.strip():
             elem.tail = i
         for elem in elem:
@@ -20,17 +20,17 @@ def indent(elem, level=0):
 
 manifest = subprocess.check_output(['repo','manifest'])
 xml_root = ElementTree.fromstring(manifest)
-lm = ElementTree.Element("manifest")
+lm = ElementTree.Element('manifest')
 projects = xml_root.findall('project')
 for project in projects:
     path = project.get('path')
     if path != None and path.find('linux') != -1:
-        lm.append(ElementTree.Element("remove-project", attrib = {
-            "name": project.get('name')
+        lm.append(ElementTree.Element('remove-project', attrib = {
+            'name': project.get('name')
         }))
 indent(lm, 0)
 raw_xml = ElementTree.tostring(lm)
-raw_xml = '<?xml version="1.0" encoding="UTF-8"?>\n' + raw_xml.decode("utf-8")
+raw_xml = '<?xml version='1.0' encoding='UTF-8'?>\n' + raw_xml.decode('utf-8')
 f = open('delete_linux.xml', 'w')
 f.write(raw_xml)
 f.close()
